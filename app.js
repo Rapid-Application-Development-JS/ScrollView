1268475322d84dffb472b99ef3d87e9ee0027c92
Anton Trofimenko <a.trofimenko@mobidev.biz>
2015-06-14 18:27:49 +0300

    Update github page

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
 define("app", function () {
+	// This is for debug only
+	function Watchman(object) {
+		var that = this;
+		this._watchable = object;
+		Object.observe(this._watchable, function (changes) {
+			changes.forEach(function (change) {
+				if (change.type in that._events === false) {
+					return;
+				}
+				if (change.name in that._events[change.type] === false) {
+					return;
+				}
+				if (typeof that._events[change.type][change.name] !== "function") {
+					return;
+				}
+				that._events[change.type][change.name](change.type, change.name, change.oldValue);
+			});
+		});
+	}
+	Watchman.prototype = {
+		_watchable: {},
+		_events: {},
+		destroy: function () {
+			Object.unobserve(this._watchable);
+			this._watchable = {};
+		},
+		subscribe: function (type, name, callback) {
+			this._events[type] || (this._events[type] = {});
+			this._events[type][name] = callback;
+		},
+		unsubscribe: function (type, name) {
+			if (type in this._events && this._events[type][name]) {
+				delete this._events[type][name];
+			}
+		}
+	};
+	//
 	var app = {};
 	app.modules = {};
 	app.data = {};
@@ -47,7 +84,7 @@ define("app", function () {
 			$bookmark.fadeOut(); // Fast scroll bookmark fadeout when scrolling stopped
 			setTimeout(function () {
 				$bookmark.hide();
-			}, 450);
+			}, 2000);
 		}
 
 		function bookmarkShow() {
@@ -90,7 +127,8 @@ define("app", function () {
 		// Create custom scroll bar
 		$scrollView.scrollBar = new app.modules["radjs-scrollview"].ScrollBar($scrollView, {
 			className: options.scrollbar,
-			direction: options.direction || "vertical"
+			direction: options.direction || "vertical",
+			smart: $scrollContent // Accepts content to watched for size changes and hides scrollbar if content fits
 		});
 		$scrollView.scroller = new app.modules["radjs-scrollview"]($scrollView, options); // Create and attach view
 		// Create and attach custom pointer events, because of: IE < 11 support, SVG elements, and links bugs
@@ -126,6 +164,7 @@ define("app", function () {
 			$scrollView.scroller.destroy();
 			$scrollView.pointer.destroy();
 			$scrollView.gesture.destroy();
+			$scrollView.scrollBar.destroy();
 		};
 		// Add content
 		var $ul = $($scrollContent).find("ul");
@@ -187,6 +226,9 @@ define("app", function () {
 			return false; // prevent default action
 		};
 		scroller._options.onMoveBefore = function (event) {
+			if (!$scrollView.pointer.isDown) {
+				return;
+			}// pointer not `pushed` or leave elemnet and back `pushed` but still funciton need to be ended
 			if (!isInScrollBarArea(event.clientX, event.clientY)) {
 				return;
 			}
@@ -207,6 +249,12 @@ define("app", function () {
 		};
 		scroller._options.onFlingBefore = function (event) {
 			return isInScrollBarArea(event.clientX, event.clientY) === false; // prevent default action if needed
+		};
+		scroller._options.onFlingBefore = function (event) {
+			return isInScrollBarArea(event.clientX, event.clientY) === false; // prevent default action if needed
+		};
+		scroller._options.onCancelAfter = scroller._options.onUpAfter = scroller._options.onLeaveAfter = function () {
+			bookmarkHide();
 		};
 	};
 	app._initScrollHorizontal = function () {
